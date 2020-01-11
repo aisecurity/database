81b1f8acce59c5cd9474138ec0df118b28dec2ea
Karen <karen5gong@gmail.com>
2020-01-10 19:27:55 -0500

    initial setup

--- pages/models.py
+++ pages/models.py
@@ -1,5 +1,12 @@
 from django.db import models
 
 # Create your models here.
-class Student(models.Model):
-    name = models.CharField(max_length=30, null=True)
+class StudentLog(models.Model):
+    student_id = models.IntegerField()
+    name = models.CharField(max_length=50)
+    time = models.DateTimeField()
+
+class UnknownLog(models.Model):
+    time = models.DateTimeField()
+    path_to_img = models.CharField(max_length=10000)
+    #path_to_img = FilePathField()
